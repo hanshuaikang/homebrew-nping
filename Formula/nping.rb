0df6fe722fba98d3c45773a5f2ee38ddb99bf444
韩数 <33194175+hanshuaikang@users.noreply.github.com>
2025-02-23 18:12:45 +0800

    feat: release v0.2.5 (#2)

--- Formula/nping.rb
+++ Formula/nping.rb
@@ -2,13 +2,13 @@
 class Nping < Formula
   desc "🏎 Nping mean NB Ping, A Ping Tool in Rust with Real-Time Data and Visualizations"
   homepage "https://github.com/hanshuaikang/Nping"
-  url "https://github.com/hanshuaikang/Nping/releases/download/v0.2.4/nping-x86_64-apple-darwin.tar.gz"
-  sha256 "512e4d92f1424b7d9bf031a96a38669bfcc6829d10d5c98ec8701bc9c28f99e7"
+  url "https://github.com/hanshuaikang/Nping/releases/download/v0.2.5/nping-x86_64-apple-darwin.tar.gz"
+  sha256 "b492113ab57fccdc9f36ee6705f1cac3693f853fb9a449ed204066c3334dacfe"
 
   # for arm64
   on_arm do
-    url "https://github.com/hanshuaikang/Nping/releases/download/v0.2.4/nping-aarch64-apple-darwin.tar.gz"
-    sha256 "bdf5bee105f0608d8f9135442dc68b34e86002ef080aab7b6e8d6ef9314733d9"
+    url "https://github.com/hanshuaikang/Nping/releases/download/v0.2.5/nping-aarch64-apple-darwin.tar.gz"
+    sha256 "7577fda2f5d094574e169b37b1b26063a418f6e5ae4dadd1d9a191c38faace9f"
   end
 
   def install
